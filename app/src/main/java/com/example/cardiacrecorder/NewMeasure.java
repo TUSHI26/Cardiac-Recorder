5a92554e21c7d2a4e9a380f37d9337cd8bbdcdff
Rakib180702 <rakib1807025@stud.kuet.ac.bd>
2022-07-21 22:50:04 +0600

    database added

--- app/src/main/java/com/example/cardiacrecorder/NewMeasure.java
+++ app/src/main/java/com/example/cardiacrecorder/NewMeasure.java
@@ -1,18 +1,30 @@
 package com.example.cardiacrecorder;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
@@ -25,11 +37,12 @@ public class NewMeasure extends AppCompatActivity {
     EditText systolic,diastolic,pulse;
     Button submit;
     TextView date, time;
-
-    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference reference = firebaseDatabase.getReference().child("data");
-
-
+    String comment;
+   UserHelper helper=new UserHelper();
+  //  DatabaseReference reference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tushi-38c81-default-rtdb.firebaseio.com/");
+  //FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+   // DatabaseReference reference = firebaseDatabase.getReference();
+DatabaseReference reference,ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +66,7 @@ public class NewMeasure extends AppCompatActivity {
         //Toast.makeText(NewMeasure.this,currentDate+time1,Toast.LENGTH_SHORT).show();
 
         time.setText(time1);
+        reference=FirebaseDatabase.getInstance().getReference().child("data");
 
 
 
@@ -60,73 +74,76 @@ public class NewMeasure extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String sys =systolic.getText().toString();
-                String dias=diastolic.getText().toString();
-                String pul=pulse.getText().toString();
-                String comment;
+               // reference.child("data").addListenerForSingleValueEvent(new ValueEventListener() {
 
-                Integer sysvalue = Integer.parseInt(sys);
-                Integer diasvalue = Integer.parseInt(dias);
-                Integer pulvalue = Integer.parseInt(pul);
+                        String sys =systolic.getText().toString();
+                        String dias=diastolic.getText().toString();
+                        String pul=pulse.getText().toString();
 
-                if(sysvalue<=150 && sysvalue>=100 && diasvalue>=60 && diasvalue<=100 && pulvalue<=100) {
-                    HashMap<String, String> userdata = new HashMap<>();
-                    userdata.put("Date", currentDate);
-                    userdata.put("Time", time1);
-                    userdata.put("systolic", sys);
-                    userdata.put("diastolic", dias);
-                    userdata.put("pulse", pul);
+                        Integer sysvalue = Integer.parseInt(sys);
+                        Integer diasvalue = Integer.parseInt(dias);
+                        Integer pulvalue = Integer.parseInt(pul);
 
-
-
-                    if(sysvalue<=110 || diasvalue<=70){
-                        comment = "low pressure";
-                        userdata.put("comment",comment);
-                    }
-
-                    else if(sysvalue<=120 || diasvalue<=80){
-                        comment = "normal";
-                        userdata.put("comment",comment);
-
-                    }
-                    else if( sysvalue<= 130 || diasvalue<=90  )
-                    {
-                        comment = "high pressure";
-                        userdata.put("comment", comment);
-
-                    }
-                    else if (sysvalue<=140 || diasvalue <=100){
-                        comment = "high pressure stage 1";
-                        userdata.put("comment",comment);
-                    }
-                    else if (sysvalue<=150 || diasvalue <=100) {
-                        comment = "high pressure stage 2";
-                        userdata.put("comment", comment);
-                    }
+                        if(sysvalue<=150 && sysvalue>=100 && diasvalue>=60 && diasvalue<=100 && pulvalue<=100) {
+                           /* HashMap<String, String> userdata = new HashMap<>();
+                            userdata.put("Date", currentDate);
+                            userdata.put("Time", time1);
+                            userdata.put("systolic", sys);
+                            userdata.put("diastolic", dias);
+                            userdata.put("pulse", pul);*/
 
 
-                    String key=currentDate+time;
-                    String key1 = key.replaceAll("\\s", "");
-                    reference.child(key1).setValue(userdata);
+
+                            if(sysvalue<=110 || diasvalue<=70){
+                                comment = "low pressure";
+
+                            }
+
+                            else if(sysvalue<=120 || diasvalue<=80){
+                                comment = "normal";
 
 
-                    Toast.makeText(NewMeasure.this,  "Data added", Toast.LENGTH_SHORT).show();
-                    systolic.setText("");
-                    diastolic.setText("");
-                    pulse.setText("");
+                            }
+                            else if( sysvalue<= 130 || diasvalue<=90  )
+                            {
+                                comment = "high pressure";
 
+
+                            }
+                            else if (sysvalue<=140 || diasvalue <=100){
+                                comment = "high pressure stage 1";
+
+                            }
+                            else if (sysvalue<=150 || diasvalue <=100) {
+                                comment = "high pressure stage 2";
+
+                            }
+                            else{
+                                Toast.makeText(NewMeasure.this,"Invalid data", Toast.LENGTH_SHORT).show();
+
+                            }
+                    helper.setSystol(sys);
+                            helper.setDiastol(dias);
+                            helper.setPuls(pul);
+                            helper.setDate(currentDate);
+                            helper.setSystol(sys);
+                            helper.setTime(time1);
+                            helper.setTime(comment);
+                            reference.push().setValue(helper);
+                            Log.e(TAG, "onClick: "+helper.getDate() );
+
+                   /*    reference.child(sys).setValue(sys);
+                        reference.child(sys).setValue(dias);
+                        reference.child(sys).setValue(pul);
+                        reference.child("data").child(sys).setValue(time1);
+                            reference.setValue( currentDate);
+                            reference.child(sys).setValue(comment);
+
+                        finish();*/
+
+                    }}
+
+                    });
 
                 }
-
-                else{
-                    Toast.makeText(NewMeasure.this,"Invalid data", Toast.LENGTH_SHORT).show();
-
-                }
-
-                Intent intent = new Intent(NewMeasure.this,History.class);
-
-
             }
-        });
-    }
-}
--- app/src/main/java/com/example/cardiacrecorder/homepage.java
+++ app/src/main/java/com/example/cardiacrecorder/homepage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class homepage extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
+        Toast.makeText(homepage.this,"data cinnected",Toast.LENGTH_SHORT).show();
         Button createMeasure= findViewById(R.id.createNewMeasure);
         createMeasure.setOnClickListener(new View.OnClickListener() {
             @Override
